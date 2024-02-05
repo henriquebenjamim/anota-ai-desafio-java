3e2724d870514960874bde202ece3d924b2bc074
henriquebenjamim <henriiqqq@gmail.com>
2024-02-05 15:49:21 -0300

    Initial configuration with mongo on src file

---
+++ src/test/java/com/henriquebenjamim/anotaaidesafio/AnotaAiDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henriquebenjamim.anotaaidesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotaAiDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
